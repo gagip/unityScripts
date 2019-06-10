4f1bc717c35cb365b7a4e9e925298447b10373d3
gagip <y02645@gmail.com>
2019-06-10 09:49:11 +0900

    백업용

---
+++ Control.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public float speed;
+    public float error;
+    private bool moveit = false;
+    private Vector3 targetpos;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
+            moveit = true;
+        }
+        if (moveit)
+        {
+            float dis = targetpos.x - transform.position.x;
+            if (Mathf.Abs(dis) <= error)
+            {
+                moveit = false;
+            }
+            Debug.Log(dis);
+            if (dis > 0)
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
+            else if (dis < 0)
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+             }
+        }
+    }
+}
